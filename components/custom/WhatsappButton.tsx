83b7b1a984cc086abdacdbb61a91d165ba86eef9
gclut <gc.luttjohann@gmail.com>
2026-03-06 15:35:36 -0300

    correction

--- components/custom/WhatsappButton.tsx
+++ components/custom/WhatsappButton.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { whatsappLink } from "@/constants/data";
+import waImage from "../../public/button.svg";
 
 export const WhatsappButton = () => {
   return (
@@ -11,7 +12,7 @@ export const WhatsappButton = () => {
       className="right-6 bottom-6 z-50 fixed flex justify-center items-center bg-transparent rounded-full w-14 md:w-20 h-14 md:h-20 transition"
     >
       <span className="inline-flex absolute bg-green-500 opacity-75 rounded-full w-full h-full animate-ping"></span>
-      <img src={"/button.svg"} alt="Botão do WhatsApp" />
+      <img src={} alt="Botão do WhatsApp" />
     </Link>
   );
 };
